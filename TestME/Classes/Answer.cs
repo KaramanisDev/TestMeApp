f78e48c31c27d0697056356be3ba088d92fb7dd2
Ilias Karamanis <karamanisweb+git@gmail.com>
2015-11-08 17:37:01 +0200

    Added Answer Class

---
+++ TestME/Classes/Answer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestME.Classes
+{
+    class Answer
+    {
+        private string _text;
+        private bool _correct;
+
+        public Answer(string text, bool correct)
+        {
+            this._text = text;
+            this._correct = correct;
+        }
+
+        public string text
+        {
+            get { return _text; }
+            set { _text = value; }
+        }
+
+        public bool correct
+        {
+            get { return _correct; }
+            set { _correct = value; }
+        }
+    }
+}
