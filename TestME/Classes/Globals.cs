0ade85d9a8b9d443ff66e2154e75629737b006d5
Ilias Karamanis <karamanisweb+git@gmail.com>
2015-11-08 16:43:59 +0200

    Added Globals Class

---
+++ TestME/Classes/Globals.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TestME
+{
+    public static class Globals
+    {
+        private static List<string> _tags;
+        private static User _user;
+
+        public static List<string> colTags
+        {
+            get { return _tags; }
+            set { _tags = value; }
+        }
+
+        public static User logUser
+        {
+            get { return _user; }
+            set { _user = value; }
+        }
+
+
+    }
+}
